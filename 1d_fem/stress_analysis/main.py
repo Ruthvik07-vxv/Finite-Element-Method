50f40a63d3ae47aa56ae1284cff7943628b5a6f3
Rothvik07-vxv <ruthvik07elr@gmail.com>
2026-03-24 02:53:44 +0530

    v0.0.1 of FEM solver done

--- 1d_fem/stress_analysis/main.py
+++ 1d_fem/stress_analysis/main.py
@@ -1,86 +1,86 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
+import mesh as msh
+import assembler as asm
+import solver as slv
 
-def create_mesh(n) :
-    F = np.zeros(n+1, dtype= float)
-    d = np.zeros(n+1, dtype= float)
-    k = np.zeros((n+1, n+1), dtype= float)
-    return F, d, k
+def Save_mesh(parameter, filename = "displacement_grid.txt") :
+    with open(filename, "w") as f :
+        for row in (parameter) :
+            for val in row :
+                f.write(f"{val:10.4f} ")
+            f.write("\n")
+    
+    print(f"Displacement Grid saved to {filename}")
 
-def stiffness_value(k, c, k_x, a, b) :
-    k[b, a] += c * k_x
+def save_force_grid(parameter, filename = "force_grid.txt") :
+    with open(filename, "w") as f :
+        for row in (parameter) :
+            for val in row :
+                f.write(f"{val:10.4f} ")
+            f.write("\n")
+    
+    print(f"Force Grid saved to {filename}")
 
-    return k
+def save_stiffness_matrix(parameter, filename = "stiffness_matrix.txt") :
+    with open(filename, "w") as f :
+        for row in (parameter) :
+            for val in row :
+                f.write(f"{val:10.4f} ")
+            f.write("\n")
+    
+    print(f"Stiffness Matrix saved to {filename}")
 
-def assemble_stiffness_matrix(k, i, k_x) :
-    k = stiffness_value(k, 1, k_x, i, i)
-    k = stiffness_value(k, -1, k_x, i, i+1)
-    k = stiffness_value(k, -1, k_x, i+1, i)
-    k = stiffness_value(k, 1, k_x, i+1, i+1)
-    return k
-
-def displacement_boundary_condition(d, n, deformation) :
-    d[n] = deformation
-    return d
-
-def force_boundary_condition(F, n, value) :
-    F[n] = value
-    return F
-
-def solve_displacement(k, F) :
-    d = np.linalg.solve(k, F)
-    return d
-
-def solve_force(k, d) :
-    F = np.dot(k, d)
-    return F
-
-def call_displacement_solver(n, x) :
-    F, d, k = create_mesh(n)
-    i = 0
-    while i < n :
-        k_x = float(input(f"Enter the stiffness value for element {i+1}: "))
-        k = assemble_stiffness_matrix(k, i, k_x)
-        i += 1
-    count = 0
-    while True :
-        count += 1
-        force = input(f"Enter the force value for node {count} (or 'done' to finish): ")
-        if force.lower() == 'done' :
-            break
-        F = force_boundary_condition(F, count-1, float(force))
-
-    d = solve_displacement(k, F)
-    return d
+n = (int(input("Enter the number of elements: ")) + 1)
+x = float(input("Enter the length of the rod: "))
+F, d, k, fixed_force, fixed_deformation = msh.create_mesh(n)
 
 
-def call_force_solver(n, x) :
-    F, d, k = create_mesh(n)
-    i = 0
-    while i < n :
-        k_x = float(input(f"Enter the stiffness value for element {i+1}: "))
-        k = assemble_stiffness_matrix(k, i, k_x)
-        i += 1
-    count = 0
-    while True :
-        count += 1
-        deformation = input(f"Enter the deformation value for node {count} (or 'done' to finish): ")
-        if deformation.lower() == 'done' :
-            break
-        d = displacement_boundary_condition(d, count-1, float(deformation))
+for i in range(n-1):
+    k_x = float(input(f"Enter the stiffness value for element {i+1}: "))
+    k = asm.assemble_stiffness_matrix(k, i, k_x)
 
-    F = solve_force(k, d)
-    return F
+count = 0
+while True and count < n :
+    count += 1
+    force = input(f"Enter the force value for node {count} (or 'done' to finish): ")
+    if force.lower() == 'done' :
+        break
+    F = asm.assemble_force_boundary_condition(F, fixed_force, count-1, float(force))
+    
 
-def main() :
-    n = input("Enter the number of elements: ")
-    x = input("Enter the length of the rod: ")
-    input_case = input("Enter the type of boundary condition (force/displacement): ").lower()
-    if input_case == "force" :
-        call_displacement_solver(n, x)
-    elif input_case == "displacement" :
-        call_force_solver(n, x)
+count = 0
+while True :
+    count += 1
+    deformation = input(f"Enter the deformation value for node {count} (or 'done' to finish): ")
+    if deformation.lower() == 'done' :
+        break
+    d = asm.assemble_displacement_boundary_condition(d, fixed_deformation, count-1, float(deformation))
+
+
+
+k_original = k.copy()
+
+count = 0
+for i in range(n) :
+    if fixed_deformation[i] :
+        F, k = slv.displacement_boundary_condition(F, k, d, i) 
     else :
-        print("Invalid input for boundary condition type.")
-        sys.exit(1)
+        count += 1
+
+if count >= n :
+    print("No fixed node found. Please ensure at least one node has a fixed deformation boundary condition.")
+    sys.exit(1)
+
+type_of_solver = input("Enter the type of solver (force/displacement): ").lower()
+if type_of_solver == "displacement" :
+    d = slv.solve_displacement(k, F)
+    F_real = slv.solve_force(k_original, d)
+    Save_mesh(d)
+    save_force_grid(F_real)
+    save_stiffness_matrix(k_original)
+else :
+    F = slv.solve_force(k_original, d)
+    Save_mesh(d)
+    save_force_grid(F)
+    save_stiffness_matrix(k_original)
